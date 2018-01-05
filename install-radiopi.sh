3eab5b903455db93c0eb949720ddc4a46bd196a6
Jeff Millies <millies.jeff@gmail.com>
2018-01-05 13:00:13 -0600

    color formatting on installer

--- install-radiopi.sh
+++ install-radiopi.sh
@@ -37,13 +37,14 @@ If you don't want these changes please answer 'n' to quit the installer
 echo -n "\e[0;31mInstall RadioPi? (y/n): \e[0;40m";
 read answer
 if echo "$answer" | grep -iq "^y" ;then
-    echo "\e[0;31m === Updating/Upgrading Apt === \e[0;40m";
-    apt-get update && apt-get upgrade -y;
-
     echo "\e[0;31m === Setting Hostname to 'radiopi' === \e[0;40m";
     sleep 3;
     sed -i 's/raspberrypi/radiopi/g' /etc/hosts;
     sed -i 's/raspberrypi/radiopi/g' /etc/hostname;
+    echo "\e[0;31mDone \e[0;40m";
+
+    echo "\e[0;31m === Updating .bashrc === \e[0;40m";
+    sleep 3;
     if [ -e /home/pi/.bashrc.bak ]
     then
         cp -rf /home/pi/.bashrc.bak /home/pi/.bashrc
@@ -54,14 +55,18 @@ if echo "$answer" | grep -iq "^y" ;then
     fi
     sed -i "s/#alias ll='ls -l'/alias ll='ls -al'/g" /home/pi/.bashrc;
     echo "$(cat /home/pi/.bashrc) \necho -e \"$LOGO\"" > /home/pi/.bashrc
-    echo "done";
+    echo "\e[0;31mDone \e[0;40m";
+
+    echo "\e[0;31m === Updating/Upgrading Apt === \e[0;40m";
+    apt-get update && apt-get upgrade -y;
+
 
     echo "\e[0;31m === Installing MPD MPC (Radio Player) and Apache2/PHP === \e[0;40m";
     sleep 5;
     apt-get install mpd mpc apache2 php php-curl php-dom -y;
     systemctl enable mpd;
     systemctl start mpd;
-    echo "done";
+    echo "\e[0;31mDone \e[0;40m";
 
     echo "\e[0;31m === Downloading RadioPi === \e[0;40m";
     sleep 5;
@@ -72,18 +77,18 @@ if echo "$answer" | grep -iq "^y" ;then
     rsync -av /tmp/radiopi-master/ /var/www/;
     rm -rf /tmp/radiopi-master;
     rm -rf /tmp/master.zip;
-    echo "done";
+    echo "\e[0;31mDone \e[0;40m";
 
     echo "\e[0;31m === Updating directories === \e[0;40m";
     sleep 5;
     chown mpd:www-data /var/lib/mpd/playlists;
     chown www-data:root /var/www/lib/radiopi.json;
     chmod -R g+rw /var/lib/mpd/playlists;
-    echo "done";
+    echo "\e[0;31mDone \e[0;40m";
 
     echo "\e[0;31m === Rebooting now === \e[0;40m";
-    echo "brb!";
-    sleep 5;
+    echo "\e[0;31mBRB! \e[0;40m";
+    sleep 3;
     unset IFS
     reboot now;
 else
